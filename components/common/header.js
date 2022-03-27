4c631a2d7f1ddfd7cd806e0a73546853d04cade2
Seydina <dina3903@gmail.com>
2022-03-27 01:58:15 +0100

    update

--- components/common/header.js
+++ components/common/header.js
@@ -1,25 +1,44 @@
 import React, { Component,Fragment, useState } from 'react';
 import Link from 'next/link'
 import { useRouter } from 'next/router'
-import { BellIcon } from '@heroicons/react/solid';
 import CalendarBoldIcon from '../ui/icons/calendarBoldIcon';
 import BellBoldIcon from '../ui/icons/bellBoldIcon';
+import MenuBurgerIcon from '../ui/icons/menuBurgerIcon';
+import { SideNav } from './sidebar';
+
 
 export default function Header(props) {
 
     const router = useRouter();
+    const [open, setOpen] = useState(false)
+
+    const onOpenChange = (SideNavData) => {
+        setOpen(SideNavData);
+    }
 
     return (
-        <header className="h-14 w-full bg-white flex flex-row justify-between px-6 shadow-sm" >
+        <header className="h-14 w-full bg-white flex flex-row justify-between px-[0.885rem] md:px-6 shadow-sm" >
 
             <div className='flex flex-row self-center '>
 
                 <div className='w-60 flex flex-row self-center'>
 
-                    <div className='h-8 w-8 rounded-full mr-2 self-center bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500'>
+                    <div className="flex flex-row self-center cursor-pointer">
+                        
+                        <div className="text-gray-800 self-center md:hidden mr-2">
+                            <button onClick={() => setOpen(true)} className="flex items-center px-[0.35rem] py-[0.35rem] border rounded border-gray-400 shadow focus:outline-none">
+                                <MenuBurgerIcon customClass="fill-current h-[0.85rem] w-[0.85rem] text-gray-700" />
+                            </button>
+                        </div>
+
+                        <div className='h-8 w-8 rounded-full mr-2 self-center bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500'>
+                        </div>
+
+                        <div className='text-2xl logo-text1 font-bold self-center'>Untitled</div>
+
                     </div>
 
-                    <div className='text-2xl logo-text1 font-bold self-center'>Untitled</div>
+                   
 
                 </div>
 
@@ -31,7 +50,7 @@ export default function Header(props) {
 
             <div className='flex flex-row self-center'>
 
-                <div className='h-8 rounded-md th-bg-1 text-gray-900 flex flex-row px-3 mr-3'>
+                <div className='h-8 rounded-md th-bg-1 text-gray-900 flex flex-row px-3 mr-3 hidden'>
                     <div className='w-4 h-4 self-center mr-2'>
                         <CalendarBoldIcon customClass="w-full h-full" />
                     </div>
@@ -44,11 +63,13 @@ export default function Header(props) {
                     </div>
                 </div>
 
-                <div className='w-8 h-8 th-bg-1 text-gray-600 rounded-full flex flex-row justify-center'>
-                    <img className='w-full h-full rounded-full' src="https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=4&w=256&h=256&q=60" />
+                <div className='w-8 h-8 th-bg-1 border-2 border-purple-600 px-[0.035rem] py-[0.035rem] rounded-full flex flex-row justify-center'>
+                    <img className='w-full h-full object-cover rounded-full' src="https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=4&w=256&h=256&q=60" />
                 </div>
 
             </div>
+
+            <SideNav open={open} onOpenChange={onOpenChange} />
         
         </header>
     );
--- components/common/sidebar.js
+++ components/common/sidebar.js
@@ -12,18 +12,15 @@ import SettingsBoldIcon from "../ui/icons/settingsBoldIcon";
 import InterfaceBoldIcon from "../ui/icons/interfaceBoldIcon";
 import EnvelopeBoldIcon from "../ui/icons/envelopeBoldIcon";
 import CarnetBoldIcon from "../ui/icons/carnetBoldIcon";
+import { Fragment, useState } from 'react'
+import { Dialog, Transition } from '@headlessui/react'
+import { XIcon } from '@heroicons/react/outline'
 
-
-export default function Sidebar() {
+export function Navigation(){
 
     const router = useRouter();
-    const style1 = ""
-    const style2 = ""
 
-  return (
-
-    <aside className="app-sidebar w-60 py-4 bg-white rounded-xl z-10" aria-label="Sidebar">
-
+    return(
         <div className="overflow-y-auto w-full h-full">
 
             <ul className="space-y-1">
@@ -211,8 +208,66 @@ export default function Sidebar() {
             </ul>
 
         </div>
+    )
 
-    </aside>
-        
+}
+
+export default function Sidebar() {
+  return (
+    <aside className="app-sidebar w-60 py-4 bg-white rounded-xl z-10 hidden md:block" aria-label="Sidebar">
+        <Navigation />
+    </aside>  
   );
 }
+
+
+export function SideNav({onOpenChange, open}) {
+
+    return (
+      <Transition.Root show={open} as={Fragment}>
+  
+          <Dialog as="div" className="fixed inset-0 overflow-hidden sz-30" onClose={() => onOpenChange(false)}>
+  
+              <div className="absolute inset-0 overflow-hidden">
+  
+                  <Transition.Child as={Fragment} enter="ease-in-out duration-500" enterFrom="opacity-0" enterTo="opacity-100" leave="ease-in-out duration-500" leaveFrom="opacity-100" leaveTo="opacity-0">
+                      <Dialog.Overlay className="absolute inset-0 bg-gray-500 bg-opacity-75 transition-opacity" />
+                  </Transition.Child>
+  
+                  <div className="fixed inset-y-0 left-0 max-w-full flex pr-10">
+  
+                      <Transition.Child as={Fragment} enter="transform transition ease-in-out duration-500 sm:duration-700" enterFrom="-translate-x-full" enterTo="translate-x-0" leave="transform transition ease-in-out duration-500 sm:duration-700" leaveFrom="translate-x-0" leaveTo="-translate-x-full">
+  
+                          <div className="w-screen max-w-[18rem]">
+  
+                              <div className="h-full flex flex-col bg-white">
+  
+                                  <div className="flex items-start justify-between h-16 px-4 sm:px-6">
+                                      <Dialog.Title className="text-xl xl:text-2xl font-bold text-gray-800 self-center">Untitled</Dialog.Title>
+                                      <div className="ml-3 h-7 flex items-center self-center">
+                                          <button type="button" className="-m-2 p-2 text-gray-700 hover:text-gray-300" onClick={() => onOpenChange(false)}>
+                                              <span className="sr-only">Close panel</span>
+                                              <XIcon className="h-6 w-6" aria-hidden="true" />
+                                          </button>
+                                      </div>
+                                  </div>
+  
+                                  <div className="border-t border-gray-200 border-opacity-40 py-6 sidebar-items-height overflow-auto overflow-y-scroll">
+                                    <Navigation />
+                                  </div>
+  
+                              </div>
+  
+                          </div>
+  
+                      </Transition.Child>
+                  </div>
+              
+              </div>
+  
+          </Dialog>
+  
+      </Transition.Root>
+    );
+  }
+  
