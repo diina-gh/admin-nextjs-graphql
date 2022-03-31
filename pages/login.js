4b83c4d5a5f4674a8f8f8a2cd96d0060506813f0
Seydina <dina3903@gmail.com>
2022-04-01 00:54:12 +0200

    update

--- pages/login.js
+++ pages/login.js
@@ -102,30 +102,31 @@ class Login extends Component {
         <Toaster position='top-right' />
 
 
-      <div className="absolute bottom-3 left-0 px-8 sm:px-20 w-full text-center text-[0.775rem] text-white font-normal py-2"> © Copyright 2022 <span className="text-gray-200">Untitled</span> | Created by <a className="cursor-pointer text-gray-200 hover:text-iired no-underline hover:underline transition duration-700 ease-in-out" href="https://www.linkedin.com/in/seydina-gueye-596b051b0/" target="_blank" rel="noreferrer">Seydina GUEYE</a></div>
+      <div className="absolute bottom-3 left-0 px-8 sm:px-20 w-full text-center text-[0.775rem] text-gray-900 font-medium py-2"> © Copyright 2022 <span className="text-gray-700">Untitled</span> | Created by <a className="cursor-pointer text-gray-700 hover:text-iired no-underline hover:underline transition duration-700 ease-in-out" href="https://www.linkedin.com/in/seydina-gueye-596b051b0/" target="_blank" rel="noreferrer">Seydina GUEYE</a></div>
 
-        <div className="rounded-md shadow-xl bg-gray-100 bg-opacity-75 px-8 sm:px-16 py-8 sm:py-10">
+        <div className="rounded-md shadow-xl bg-gray-500 bg-opacity-60 px-8 sm:px-16 py-8 sm:py-10">
 
           <BlockUI blocking={this.state.block} />
 
           <div>
-            <img className="mx-auto h-12 w-auto" src="favicon.ico"/>
-            <h2 className="mt-6 text-center text-xl font-bold text-gray-900">Connectez-vous à votre compte</h2>
+            {/* <img className="mx-auto h-12 w-auto" src="/favicon.ico" /> */}
+            <h1 className='text-center text-3xl font-extrabold text-purple-600'>Untitled</h1>
+            <h2 className="mt-4 text-center text-xl font-bold text-gray-900">Connectez-vous à votre compte</h2>
           </div>
           <form className="max-w-md mt-8 space-y-6 w-72 sm:w-80" method="POST" onSubmit={(e) => this.checkInput(e)}>
             <div className="rounded-md -space-y-px">
               <div className='mb-4'>
                 <label htmlFor="email-address" className="sr-only"> Identifiant</label>
-                <input value={this.state.email} onChange={(e) => this.setState({ email: e.target.value})} id="email-address" name="email" type="email" autoComplete="email" required className="h-11 relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-md focus:outline-none bg-white focus:border-0 focus:ring-2 focus:ring-purple-500 text-sm font-medium bg-opacity-75" placeholder="Adresse email"/>
+                <input value={this.state.email} onChange={(e) => this.setState({ email: e.target.value})} id="email-address" name="email" type="email" autoComplete="email" required className="h-11 relative block w-full px-3 py-2 border border-gray-300 border-opacity-20 placeholder-gray-600 text-gray-900 rounded-md focus:outline-none bg-white focus:border-0 focus:ring-2 focus:ring-purple-500 text-sm font-medium bg-opacity-20" placeholder="Adresse email"/>
               </div>
               <div className="">
                 <label htmlFor="password" className="sr-only">Mot de passe</label>
-                <input value={this.state.password} onChange={(e) => this.setState({ password: e.target.value})} id="password" name="password" type="password" autoComplete="current-password" required className="h-11 relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-md focus:outline-none bg-white focus:border-0 focus:ring-2 focus:ring-purple-500 text-sm font-medium bg-opacity-75" placeholder="Mot de passe"/>
+                <input value={this.state.password} onChange={(e) => this.setState({ password: e.target.value})} id="password" name="password" type="password" autoComplete="current-password" required className="h-11 relative block w-full px-3 py-2 border border-gray-300 border-opacity-20 placeholder-gray-600 text-gray-900 rounded-md focus:outline-none bg-white focus:border-0 focus:ring-2 focus:ring-purple-500 text-sm font-medium bg-opacity-20" placeholder="Mot de passe"/>
               </div>
             </div>
 
             <div>
-              <button type="submit" className="btn-effect1 group relative h-12 w-full flex justify-center px-4 border text-lg font-medium rounded-md text-white bg-gray-900 hover:bg-purple-600 focus:outline-none">
+              <button type="submit" className="btn-effect1 group relative h-12 w-full flex justify-center px-4 text-lg font-medium rounded-md text-white bg-purple-600 hover:bg-purple-500 focus:outline-none">
                 <span className="absolute left-0 inset-y-0 flex items-center pl-3">
                   <LockClosedIcon className="h-5 w-5 text-gray-300 group-hover:text-gray-100" aria-hidden="true" />
                 </span>
