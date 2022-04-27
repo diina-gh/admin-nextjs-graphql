98056955e1745d58ad2a142bf2fbe8a29dce0ad8
Seydina <dina3903@gmail.com>
2022-04-28 00:32:41 +0200

    update

---
+++ pages/clients/details.js
@@ -0,0 +1,41 @@
+import { useState, useEffect } from 'react'
+import Link from 'next/link'
+import { motion } from "framer-motion";
+import Header from '../../components/common/header'
+import Sidebar from '../../components/common/sidebar'
+import HeadInfo from '../../components/common/headinfo'
+
+
+export default function Index() {
+
+    return (
+        <div className="app-container h-screen">
+
+            <HeadInfo title= 'Dashboard' description='description here'/>
+            <Header/>
+
+            <div className='w-full pt-3 px-3 md:px-6 flex flex-row justify-between'>
+
+                <Sidebar />
+
+                <motion.div initial={{ opacity: 0.45, x: -150 }}  whileInView={{ opacity: 1, x: 0, transition: { duration: 0.60 }, }}>
+                    <div className='app-body rounded-xl'>
+
+                        <div className='w-full h-full bg-white rounded-xl overflow-y-scroll pt-4 pb-3'>
+
+                            
+
+                        </div>
+
+                    </div>
+                </motion.div>
+                
+
+            </div>
+
+        </div>
+    )
+}
+
+
+
