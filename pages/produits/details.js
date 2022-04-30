959f6d955e3fa2470f0cda1e837bdd0194c757bd
Seydina <dina3903@gmail.com>
2022-04-30 04:57:31 +0200

    update

--- pages/produits/details.js
+++ pages/produits/details.js
@@ -6,12 +6,16 @@ import Sidebar from '../../components/common/sidebar'
 import HeadInfo from '../../components/common/headinfo'
 import { DotsHorizontalIcon } from '@heroicons/react/solid';
 import { xof } from '../../libs/util';
+import GroupBoldIcon from '../../components/ui/icons/groupBoldIcon';
+import { getProducts } from '../../hooks/product';
+import { useDebouncedCallback } from 'use-debounce';
+import {Pagination as Paginate}  from "react-pagination-bar"
 import 'react-pagination-bar/dist/index.css'
 import ChevronLeftIcon from '../../components/ui/icons/chevronLeftIcon';
 import ChevronRightIcon from '../../components/ui/icons/chevronRightIcon';
 import DoubleChevronLeftIcon from '../../components/ui/icons/doubleChevronLeftIcon';
 import DoubleChevronRightIcon from '../../components/ui/icons/doubleChevronRightIcon';
-import GroupBoldIcon from '../../components/ui/icons/groupBoldIcon';
+import { capitalize } from '../../libs/util';
 // Import Swiper React components
 import { Swiper, SwiperSlide } from "swiper/react";
 // import required modules
@@ -24,6 +28,36 @@ import "swiper/css/navigation";
 
 
 export default function Index() {
+
+    const [take, setTake] = useState(6);
+    const [page, setPage] = useState(1);
+    const [filter, setFilter] = useState('')
+    const [direction, setDirection] = useState('asc')
+    const [orderBy, setOrderBy] = useState({"id": direction})
+
+    const fields = {"productName": true, "productDesc": true, "productUnit": true, "productUnitprice": true, "productUnitweight": true, "productActivated": true, "productBrand": true, "productBrandName": true, "productCategory": true, "productCategoryName": true, "productImage": true, "imageUrl": true, "imageImageref": true, "productInventory": true, "productInventoryQuantity": true}
+    var { items, isLoading, isError, mutate } = getProducts(page,take,filter, orderBy, fields )
+
+    const refetch = useDebouncedCallback(
+        (newPage, newFilter = null, newOrder = null ) => {
+            if(newPage && newPage != page){
+                setPage(newPage)
+                mutate({...items, page:newPage})
+            } 
+            if(newFilter){
+                setFilter(newFilter)
+                mutate({...items, filter:newFilter})
+            }
+            if(newOrder){
+                setOrderBy(newOrder)
+                mutate({...items, orderBy:newOrder})
+            }
+        }, 680
+    );
+
+    if (isError) console.log("The error here ", isError)
+    if (isLoading) console.log("loading...")
+    if(items) console.log("Informations => ", items)
 
     return (
         <div className="app-container h-screen">
@@ -133,7 +167,7 @@ export default function Index() {
                                     </div>
                                 </div>
 
-                                <div className='text-[15.5px] font-semibold text-purple-700 truncate mt-8 mb-3 ml-0.5 px-6'>Statistiques</div>
+                                <div className='text-[15.5px] font-semibold text-purple-700 truncate mt-8 mb-5 ml-0.5 px-6'>Statistiques</div>
                                 
                                 <div className='w-full grid grid-cols-4 grid-flow-row gap-5 overflow-hidden px-6'>
                                     <motion.div initial={{ opacity: 0.35, x:-50 }} whileInView={{ opacity: 1, x: 0, transition: { duration: 1.05 }, }}>
@@ -216,7 +250,61 @@ export default function Index() {
 
                                 <div className='text-[15.5px] font-semibold text-purple-700 truncate mt-8 mb-3 ml-0.5 px-6'>Produits associés</div>
 
-                                <div className='mb-6'></div>
+                                <div className='w-full px-6 mb-6'>
+                                    {(isLoading || items?.page != null || items?.filter != null || items?.orderBy != null ) &&
+                                        <div className='h-[11.85rem] w-full flex flex-row justify-center'>
+                                            <div className='h-10 self-center'><img className='h-full' src="../spinner.gif" /></div>
+                                        </div>                                                  
+                                    }
+
+                                    {(items && items.products && items?.page == null && items?.filter == null && items?.orderBy == null ) &&
+                                        <div className="w-full grid grid-cols-6 gap-4">
+                                            {items.products.products.map((item, i) => (
+                                                <div key={i} className="w-full">
+                                                    <motion.div initial={{ opacity: 0, y: ( Math.random() + i * 5) }} whileInView={{ opacity: 1, y: 0, transition: { duration: 1.05 }, }}>
+                                                        <div className="w-full pt-1 pb-5 rounded-xl bg-gray-200 bg-opacity-80 cursor-pointer relative parent-layer">
+
+                                                            <div className='w-full image-layer-00 flex flex-row justify-center'>
+                                                                <div className='image-layer-01 self-center'>
+                                                                    <img src={item.images[0]?.url} />
+                                                                </div>
+                                                            </div>
+
+                                                            <div className='w-full mt-2 px-3'>
+                                                                <div className='w-full text-gray-500 text-[0.7rem] font-medium truncate'>{capitalize(item.category?.name)}</div>
+                                                                <div className='w-full flex mt-1'>
+                                                                    <div className='self-center w-full'>
+                                                                        <div className='w-full main-item text-gray-900 cursor-pointer text-[0.87rem] font-semibold truncate transition duration-700 ease-in-out'>{capitalize(item.name)}</div>
+                                                                        <div className='w-full text-gray-800 text-[0.71rem] font-medium mt-1'> {new Intl.NumberFormat('fr-FR', {style: 'currency', currency:'XOF'}).format(item.unitprice)}</div>
+                                                                    </div>
+                                                                </div>
+                                                            </div>
+
+                                                        </div>
+
+                                                    </motion.div>
+                                                </div>
+                                            ))}
+                                        </div>
+                                    }
+                    
+                                    <div className='w-full flex flex-row justify-end mt-5'>
+                                        {items?.products != null &&
+                                            <Paginate 
+                                                initialPage={page} 
+                                                itemsPerPage={take} 
+                                                onPageСhange={(pageNumber) => refetch(pageNumber)} 
+                                                totalItems={items?.products?.count}  
+                                                pageNeighbours={2} 
+                                                startLabel= {<DoubleChevronLeftIcon customClass="w-3 h-3"/>}
+                                                endLabel={<DoubleChevronRightIcon customClass="w-3 h-3"/>}
+                                                nextLabel={<ChevronRightIcon customClass="w-3 h-3"/>}
+                                                prevLabel={<ChevronLeftIcon customClass="w-3 h-3"/>}
+                                                customClassNames={{rpbItemClassName:'pg-btn', rpbItemClassNameActive:'pg-active-btn',}}
+                                            />
+                                        }
+                                    </div>
+                                </div>
 
                             </div>
 
